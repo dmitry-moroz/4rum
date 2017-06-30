fabff6c27ff21275909b2a683cff1fa4c859c9bb
Dmitriy Moroz <d_moroz@wargaming.net>
2017-06-30 13:48:02 +0300

    dbshell has been added

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
+from flask_dbshell import DbShell
 
 from app import create_app, db
 from app.models import User, Role, Permission
@@ -14,8 +15,15 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Permission=Permission)
-manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def dbshell():
+    """Run DB shell."""
+    shell = DbShell(url=app.config['SQLALCHEMY_DATABASE_URI'])
+    shell.run_shell()
 
 
 @manager.command
