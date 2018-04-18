19282f39a2cfaa54c2324ca364abd0c2a0ce4713
Dmitriy Moroz <d_moroz@wargaming.net>
2018-04-18 22:57:47 +0300

    POST with csrf for vote method

--- forum/main/__init__.py
+++ forum/main/__init__.py
@@ -2,10 +2,11 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
+from flask_wtf.csrf import generate_csrf  # noqa: E402
+from flask_babel import format_datetime, format_timedelta  # noqa: E402
+
 from . import views, errors  # noqa: E402, F401
 from ..models import Permission  # noqa: E402
-from flask_babel import format_datetime, format_timedelta  # noqa: E402
-from .views import get_locale  # noqa: E402
 
 
 @main.app_context_processor
@@ -14,5 +15,6 @@ def inject_variables_for_jinja():
         Permission=Permission,
         format_datetime=format_datetime,
         format_timedelta=format_timedelta,
-        get_locale=get_locale,
+        get_locale=views.get_locale,
+        generate_csrf=generate_csrf,
     )
